8fa728e0805629833f5e7013beea84d3b9da9761
Daniel Krebs <d.krebs@enlyze.com>
2021-11-07 18:52:30 +0100

    increase grid size to 7x7 and probe one more time

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1218,7 +1218,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 3
 #define EXTRA_PROBING    1
 
 /**
@@ -1591,7 +1591,8 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 7
+
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
