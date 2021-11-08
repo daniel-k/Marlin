9ec9ae19e8212d93af7314a1d975711755b700eb
Daniel Krebs <d.krebs@enlyze.com>
2021-11-08 22:57:59 +0100

    tune X and Y limits and bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1358,15 +1358,15 @@
 
 // The size of the printable area
 #define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define Y_BED_SIZE 219
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -10 // clips in the middle of the bed limit printing area.
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 210
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
